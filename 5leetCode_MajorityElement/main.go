b14eef8219cd0f5eb5d3d17af6a2761c39af5714
Abir Roy <abir.roy.03.07@gmail.com>
2023-09-04 14:44:11 +0600

    Find the Mode; highest frequecy

---
+++ 5leetCode_MajorityElement/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{2, 2, 1, 1, 1, 2, 2}
+	fmt.Println(majorityElement(nums))
+
+}
+
+func majorityElement(nums []int) int {
+	l := 0
+
+	return l
+}
